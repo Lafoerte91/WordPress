043cfe1b120b73da5f567a996b4ba9ff2b6bedbf
Lafoerte91 <lafoerte@gmail.com>
2024-05-28 21:22:30 +0300

    подключаем разметку

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
 <?php 
-
-echo "I Header!";
+?>
+<!DOCTYPE html>
+<html <html <?= language_attributes(  ); ?>>
+<head>
+  <meta charset="<?= bloginfo( 'charset' ) ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php wp_head(  ); ?> 
+</head>
+<body <?= body_class(  ); ?>> 
+  
