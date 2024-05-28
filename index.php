95bac259c41bdb2dc3d8219cae9c979d09e876cc
Lafoerte91 <lafoerte@gmail.com>
2024-05-28 16:43:41 +0300

    меняем превью и добавляем перебор постов

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php 
 
+if(have_posts(  )) : // проверка на наличие постов
+  while(have_posts(  )) : the_post(  ); // перебор постов
+    the_title(); // заголовок поста
+    the_content(); // содержание поста
+  endwhile;
+endif;
+?>
+
