7a4d745ce44376ea6c35e1c16a763021c43bc555
Lafoerte91 <lafoerte@gmail.com>
2024-07-07 19:17:33 +0300

    header и footer

---
+++ footer.php
@@ -0,0 +1,34 @@
+<!-- Team -->
+<section class="team">
+    <div class="container">
+      <div class="block">
+        <h2 class="block_title">Our talanted mamber</h2>
+        <p class="block_text">Increase your insight and make the better experience with our valuable services.</p>
+      </div>
+      <div class="team_items">
+        <div class="team_item">
+          <img class="team_img" src="img/team1.png" alt="Sarah Gilbert">
+          <div class="team_block">
+            <h4 class="team_title">Sarah Gilbert</h4>
+            <p class="team_text">CEO & Founder DigiXine</p>
+          </div> 
+        </div>
+        <div class="team_item">
+          <img class="team_img" src="img/team2.png" alt="Stephen Joe">
+          <div class="team_block">
+            <h4 class="team_title">Stephen Joe</h4>
+            <p class="team_text">Social Media Manager</p>
+          </div> 
+        </div>
+        <div class="team_item">
+          <img class="team_img" src="img/team3.png" alt="Danie Rogue">
+          <div class="team_block">
+            <h4 class="team_title">Danie Rogue</h4>
+            <p class="team_text">Senior UX Writer</p>
+          </div> 
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
