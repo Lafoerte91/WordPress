54350eb9b79da00c4215b1e3ec3dd9a051089896
Lafoerte91 <lafoerte@gmail.com>
2024-08-18 21:27:21 +0300

    cfs

--- header.php
+++ header.php
@@ -18,8 +18,8 @@
   <header class="header">
     <div class="container">
       <a href="<?php echo home_url(); ?>">Home</a>
-      <h1 class="header_title">Digital Marketing Agency</h1>
-      <p class="header_text">Award winning digital marketing agency in Singapore. A home-grown company since 2018.</p>
+      <h1 class="header_title"><?php echo CFS()->get('header_title'); ?></h1>
+      <p class="header_text"><?php echo CFS()->get('header_text'); ?></p>
       <button class="header_button">Find out more</button>
     </div>
   </header>
